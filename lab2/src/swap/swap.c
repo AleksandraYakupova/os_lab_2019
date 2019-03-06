c5bf2716b7ec7b69c3ef852a0c2c06a520757ef5
AleksandraYakupova <maxlex047@gmail.com>
2019-03-06 20:07:37 +0000

    добавлен swap.c

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -1,6 +1,13 @@
 #include "swap.h"
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+    char *tmp = (char *)malloc(sizeof(char));
+    strcpy(tmp, left);
+    strcpy(left, right);
+    strcpy(right, tmp);
+    free(tmp);
 }
